c67155ef52e0fef2265ad760889860345d55d5f6
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2020-09-17 22:38:23 -0500

    add logic

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Create super user
  - docker-compose run app sh -c "python manage.py createsuperuser"
+
+ ## Link logic
+ - https://repl.it/repls/IncomparableDodgerblueComment
--- app/items/utils.py
+++ app/items/utils.py
@@ -1,1 +1,13 @@
 
+
+def convert_nested_list_in_flattens_list(nested):
+    """Convert Nested list in flattens list"""
+    list_end = []
+    for element in nested:
+        if type(element) == list:
+            list_end.extend( 
+                convert_nested_list_in_flattens_list(element) 
+            )
+        else:
+            list_end.append(element)
+    return list_end
