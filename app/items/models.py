4999f154d543df8e43ad78012f7205e05890988f
Yoandre Saavedra <yoandre.saavedra@gmail.com>
2020-09-17 22:04:27 -0500

    add pytest

--- app/items/models.py
+++ app/items/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Item(models.Model):
+    result = ArrayField(models.IntegerField())
+
+    def __str__(self):
+        return f'{self.result}'
